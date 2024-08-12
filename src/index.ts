375a2173c710dc0b3f2801df0605a2a7581a2414
praskoson <prasinar@gmail.com>
2024-08-12 11:12:46 +0200

    chore: remove unused structs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { RpcConnection } from './rpc';
-export { Actions } from './constants';
+export { Action } from './constants';
 export { AccountInfoResult } from './sdk/account-info-result';
 export { Pubkey } from './sdk/pubkey';
 export { UtxoInfo, UtxoMeta } from './sdk/utxo';
