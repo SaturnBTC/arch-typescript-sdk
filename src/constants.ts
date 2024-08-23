5a5e15d2c01590830425cc5a675d1e1c8714bc8f
Marcos Collado Martín <mcolladomcm@gmail.com>
2024-08-23 13:14:26 +0200

    Remove duplicate cosntants

--- src/constants.ts
+++ src/constants.ts
@@ -12,8 +12,6 @@ export enum Action {
   GET_BEST_BLOCK_HASH = 'get_best_block_hash',
   GET_PROCESSED_TRANSACTION = 'get_processed_transaction',
   GET_ACCOUNT_ADDRESS = 'get_account_address',
-  GET_BLOCK_COUNT = 'get_block_count',
-  GET_BLOCK_HASH = 'get_block_hash',
   GET_ACCOUNT_INFO = 'get_account_info',
 }
 
